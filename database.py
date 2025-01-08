a58f46b35da4778b49bf04af346ef1add87f7b06
Wellynton.spagnol <wellyntonspagnol10@gmail.com>
2025-01-08 15:44:25 -0300

    update config database

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from databases import Database
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost:5432/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://udbfacialhom:Dw^X3vpMel30HO%S@dbdevprisma.postgres.database.azure.com:5432/dbfacial_hom")
 
 database = Database(DATABASE_URL)
